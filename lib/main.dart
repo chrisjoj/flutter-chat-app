e4923f08bb9750f094162aa70f2fe9c52e987bda
Chris Locker <Chris Joj>
2018-01-07 14:49:12 +0000

    Support longer chat messages

--- lib/main.dart
+++ lib/main.dart
@@ -115,8 +115,7 @@ class ChatMessage extends StatelessWidget {
   final AnimationController animationController;
 
   @override
-  Widget build(BuildContext context) {
-    return new SizeTransition(
+  Widget build(BuildContext context) => new SizeTransition(
         sizeFactor: new CurvedAnimation(
             parent: animationController, curve: Curves.easeOut),
         axisAlignment: 0.0,
@@ -129,18 +128,19 @@ class ChatMessage extends StatelessWidget {
                 margin: const EdgeInsets.only(right: 16.0),
                 child: new CircleAvatar(child: new Text(_name[0])),
               ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(_name, style: Theme.of(context).textTheme.subhead),
-                  new Container(
-                    margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(text),
-                  ),
-                ],
+              new Expanded (
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name, style: Theme.of(context).textTheme.subhead),
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(text),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ));
-  }
 }
